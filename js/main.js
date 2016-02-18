0cab72be202b96791187a5896157279483e494d7
lugDroid <lugdroid@gmail.com>
2016-02-18 20:45:21 +0000

    Improved design and functions

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 $(document).ready(function() {
+  // place focus into form
+  $("#search-box").focus();
+
   // var searchQuery = "https://en.wikipedia.org//w/api.php?action=query&format=json&prop=info%7Clinks&generator=search&gsrsearch=";
   var searchQuery ="https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=";
 
   // wait for enter key pressing
   $(document).keypress(function(e) {
     if (e.which == 13) {
-      var searchTerm = $("#search-box").val();
-      $.get(searchQuery + searchTerm, function(data) {
-        // console.log(data.toString());
-        console.log(searchQuery + searchTerm);
+      // remove previous search results
+      $(".result").remove();
 
-        // show search results
-        // $("#site-wrapper").append("<p>" + data.query.pages["33226951"].title + "</p>");
+      // get user input
+      var searchTerm = $("#search-box").val();
+
+      // wikipedia search
+      $.get(searchQuery + searchTerm, function(data) {
         for (var i = 0; i < data.query.search.length; i++) {
-          $("#site-wrapper").append("<a href='https://en.wikipedia.org/wiki/'" + data.query.search[i].title + ">" + data.query.pages[i].title + "</a>");
+          var html = "<div class='result'><a href='https://en.wikipedia.org/wiki/";
+          html += data.query.search[i].title;
+          html += "' target='_blank'><h4>";
+          html += data.query.search[i].title + "</h4>";
+          html += "<p>" + data.query.search[i].snippet + "</p>";
+          html += "</a></div>";
+          $("#results-wrapper").append(html);
+          // $("#results-wrapper").append("<p><a href='https://en.wikipedia.org/wiki/" + data.query.search[i].title + "' target='_blank'>" + data.query.search[i].title + "</a></p>")
         }
       });
+    }
+  });
+
+  $(document).keypress(function(e) {
+    if (e.which == 27) {
+      console.log("esc");
+      $("#search-box").val("");
     }
   });
 });
