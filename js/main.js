b5993d93a8e0bbd4212c2ba7d3d7aa45ecdb884c
lugDroid <lugdroid@gmail.com>
2016-03-02 20:03:34 +0000

    Added search button

--- js/main.js
+++ js/main.js
@@ -4,35 +4,47 @@ $(document).ready(function() {
 
   var searchQuery = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=20&callback=?&search=";
 
-  // wait for enter key pressing
+  // key press handler
   $("#search-box").keypress(function(e) {
     if (e.which == 13) {
-      // remove previous search results
-      $(".result").remove();
-
-      // get user input
-      var searchTerm = $("#search-box").val();
-
-      // wikipedia search
-      $.getJSON(searchQuery + searchTerm, function(data) {
-        for (var i = 0; i < data[1].length; i++) {
-          var html = "<div class='result'><a href='https://en.wikipedia.org/wiki/";
-          html += data[1][i]; // add title
-          html += "' target='_blank'><h4>";
-          html += data[1][i] + "</h4>";
-          html += "<p>" + data[2][i] + "</p>";
-          html += "</a><div class='line-separator'></div></div>";
-          $("#results-wrapper").append(html);
-        }
-      });
+      searchWiki();
     }
   });
 
   // delete search box contents on 'esc' key pressing
   $(document).keypress(function(e) {
-    if (e.which == 27) {
-      console.log("esc");
+    if (e.keyCode == 27) {
       $("#search-box").val("");
     }
   });
+
+  // search button handler
+  $("#search-btn").click(searchWiki);
+
+  // search and display results
+  function searchWiki() {
+    // remove previous search results
+    $(".result").remove();
+
+    // get user input
+    var searchTerm = $("#search-box").val();
+
+    // wikipedia search
+    $.getJSON(searchQuery + searchTerm, function(data) {
+      // if no results found
+      if (data[1].length === 0) {
+        console.log("no results");
+        $("#results-wrapper").append("<h4 class='result'>Sorry, no results found!</h4>");
+      }
+      for (var i = 0; i < data[1].length; i++) {
+        var html = "<div class='result'><a href='https://en.wikipedia.org/wiki/";
+        html += data[1][i]; // add title
+        html += "' target='_blank'><h4>";
+        html += data[1][i] + "</h4>";
+        html += "<p>" + data[2][i] + "</p>";
+        html += "</a><div class='line-separator'></div></div>";
+        $("#results-wrapper").append(html);
+      }
+    });
+  }
 });
